42ded3170027d766f4ec600b6a337205486ff765
almsam <44277999+almsam@users.noreply.github.com>
2025-12-02 17:22:56 -0800

    plot

--- AuditGraph.py
+++ AuditGraph.py
@@ -16,3 +16,10 @@ data = { "Metric": [
 Color = ["#ff0000", "#ff8000", "#eecc00", "#000000", "#00bb44", "#2244ff", "#8040ff", "#000000", "#aa5000"]
 
 df = pd.DataFrame(data)
+
+plt.figure(figsize=(12, 6)); sns.barplot(data=df, x="Metric", y="Score", palette=Color)
+
+plt.title("Audit Evaluation Scores", fontsize=16); plt.xlabel("Metric", fontsize=12)
+plt.ylabel("Score out of 10", fontsize=12); plt.xticks(rotation=82, ha="right")
+
+plt.ylim(0, 10); plt.tight_layout(); plt.show()
